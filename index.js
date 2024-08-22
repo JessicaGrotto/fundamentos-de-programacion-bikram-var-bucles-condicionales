748cad40f439b2117291cad7ea5fa79afdbfb880
JessicaGrotto <jessicalgrotto@gmail.com>
2024-08-22 09:21:37 +0200

    finalizado

---
+++ index.js
@@ -0,0 +1,71 @@
+//Pair Programming
+//Variables
+let variableSinValor;
+let booleano1 = true;
+let booleano2 = false;
+const PI = 3.14;
+const TAU = 2*PI;
+
+//Booleanos
+const booleanoAnd = booleano1 && booleano2;
+const booleanoNot = !booleano1;
+const booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2));
+
+//Operadores
+const incrementarDesp = 2++;
+const incrementarAntes = ++2;
+
+//Bucles
+const contarHasta10_2 = 0
+for (let i = 0; i<10; i++){
+    contarHasta10_2++;
+}
+console.log(contarHasta10_2);
+
+let postI = 0;
+let postJ = 0;
+for(let i=o; i<=10; i++){
+    postI =+ postJ++;
+};
+
+let sumaPares = 0;
+for(let i= 0; i<0; i++){
+    if(i%2 === 0) sumaPares+=i;
+};
+
+//Proyecto Individual
+//Variables
+let variableValorNumerico = 10;
+const MiNombre = "Jéssica"
+const MiNumeroFav = 13;
+
+//Booleanos
+const booleanoOr = booleano1 || booleano2;
+const booleanoMix1 = (booleano1 && (TAU/2 == PI)) || (variableValorNumerico >= MiNumeroFav);
+const seisNoEsNueve = 6 !== 9;
+const booleanoMix2 = variableValorNumerico > 0 && variableValorNumerico < -(MiNumeroFav * TAU);
+
+//Operadores
+const valorSuma = MiNumeroFav + variableValorNumerico;
+const valorResta = MiNumeroFav - variableValorNumerico;
+const valorMultiplicacion = MiNumeroFav * variableValorNumerico;
+const valorDivision = MiNumeroFav/3;
+
+//Bucles
+let contarHasta10 = 0;
+while(contarHasta10<=10){
+    contarHasta10 += 1
+};
+
+let preI = 0;
+let preJ = 0;
+for(let i=0; i<=10; i++){
+    preI += ++preJ
+};
+
+let sumarImpares = 0;
+for(let i=0; i < 10; i++){
+    if(sumarImpares % 2 !== 0){
+        sumarImpares =+ i;
+    }
+};
